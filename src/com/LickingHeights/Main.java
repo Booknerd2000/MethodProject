86b804b86cdc939855f7a26f5541cbd4f4d12f26
DenisAjuah <Denis.ajuah.2021@student.lhschools.org>
2019-02-28 08:12:57 -0500

    Added Scanner

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,10 @@
 package com.LickingHeights;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner keyboard;
+        keyboard = new Scanner(System.in);
 	// write your code here
     }
 }
